ce9169e96ddb90411855aa3f445393f5f2227c44
wchen1994 <wchen1994@github.com>
2017-04-15 23:12:13 +1000

    Added main function

---
+++ Framework/Framework/Window.h
@@ -0,0 +1,6 @@
+#pragma once
+class Window {
+public:
+	static constexpr int ScreenWidth = 800;
+	static constexpr int ScreenHeight = 600;
+};
---
+++ Framework/Framework/main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+#include "Window.h"
+
+int main() {
+	sf::RenderWindow wnd(sf::VideoMode(Window::ScreenWidth, Window::ScreenHeight),L"Application");
+	while (wnd.isOpen()) {
+		sf::Event event;
+		while (wnd.pollEvent(event)) {
+			sf::Event::EventType eventType = event.type;
+			switch (eventType) {
+			case sf::Event::Closed:
+				wnd.close();
+				break;
+			}
+		}
+
+		wnd.clear();
+		wnd.display();
+	}
+}
